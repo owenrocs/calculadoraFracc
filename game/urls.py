7689f7337a7c9584d2df49fb442e603b6a4e5f02
owenrocs <102175853+owenrocs@users.noreply.github.com>
2023-03-02 09:47:24 -0600

    Initial commit

---
+++ game/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('proceso', views.proceso, name = 'proceso'),
+    path('bienvenida', views.bienvenida, name = 'bienvenida'),
+    path('suma', views.suma, name = 'suma'),
+    path('resta', views.resta, name = 'resta'),
+    path('multiplicacion', views.multiplicacion, name = 'multiplicacion'),
+    path('division', views.division, name = 'division'),
+]
---
+++ game/views.py
@@ -0,0 +1,99 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from fractions import Fraction
+from django.views.decorators.csrf import csrf_exempt
+from json import loads, dumps
+# loads -> convierte un string a un objeto JSON
+# dumps -> convierte un objeto JSON a un string
+
+# Create your views here.
+class Fraccion:
+    def __init__(self, numerador, denominador):
+        self.numerador = numerador
+        self.denominador = denominador
+    def toJSON(self):
+        return dumps(self, default=lambda o: o.__dict__, sort_keys=False, indent=4)
+    
+
+def index(request):
+    #return HttpResponse('<h1> Hola Mundo! </h1>')
+    return render(request, 'index.html')
+
+
+def proceso(request):
+    nombre = request.POST['nombre']
+    nombre = nombre.upper()
+    return HttpResponse('Hola ' + nombre)
+
+def bienvenida(request):
+    letrero = "Bienvenida"
+    return HttpResponse(letrero)
+
+@csrf_exempt
+def suma(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+    num_resultado = (den1 * num2) + (den2 * num1)
+    den_resultado = den1 * den2
+    resultado = Fraccion(num_resultado,den_resultado)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json, content_type = "text/json-comment-filtered")
+
+@csrf_exempt
+def resta(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+    num_resultado = (num1 * den2) - (den1 * num2)
+    den_resultado = den1 * den2
+    #resultado = Fraccion(num_resultado,den_resultado)
+    resultado = Fraction(num_resultado,den_resultado)
+    #resultado_json = resultado.toJSON()
+    #return HttpResponse(resultado_json, content_type = "text/json-comment-filtered")
+    return HttpResponse(resultado, content_type = "text/json-comment-filtered")
+    ##### IMPLEMENTACION DE LIBRERIA FRACTIONS #####  
+
+#def multiplicacion(request):
+#    #Obtener parametros de un endpoint
+#    p = request.GET['p']
+#    q = request.GET['q']
+#    r = int(p) * int(q)
+#    return HttpResponse("Multiplicacion: " + p + " * " + q + " = " + str(r))
+
+@csrf_exempt
+def multiplicacion(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+    num_resultado = num1 * num2
+    den_resultado = den1 * den2
+    resultado = Fraccion(num_resultado,den_resultado)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json, content_type = "text/json-comment-filtered")
+
+@csrf_exempt
+def division(request):
+    #Division por medio del metodo POST
+    #Comando para usar formato unicode -> body codificado en UTF-8
+    body_unicode = request.body.decode('utf-8')
+    #Cargar el cuerpo de la peticion
+    body = loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+    num_resultado = num1 * den2
+    den_resultado = den1 * num2
+    resultado = Fraccion(num_resultado,den_resultado)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json, content_type = "text/json-comment-filtered")
