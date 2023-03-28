af85543d5510beb6ff6b23c2ecb3eeb9cb5ce61d
owenrocs <102175853+owenrocs@users.noreply.github.com>
2023-03-28 12:46:51 -0600

    grafica

--- game/views.py
+++ game/views.py
@@ -209,20 +209,75 @@ def valida_usuario(request):
     return HttpResponse('{"estatus": true}')
 
 def grafica(request):
-    h_var = 'x'
-    v_var = 'y'
-    data = [['id_usuario', 'minutos_jugados', 'puntaje'],
-            ['1', 10, 20],
-            ['2', 20, 30],
-            ['3', 30, 40],
-            ['4', 40, 50],
-            ['5', 50, 60],
-            ['6', 60, 70]]
-    # loop to append list of three random values to data object
-    for i in range(7):
+    #h_var : The title for horizontal axis
+    h_var = 'X'
+
+    #v_var : The title for horizontal axis
+    v_var = 'Y'
+
+    #data : A list of list which will ultimated be used 
+    # to populate the Google chart.
+    data = [[h_var,v_var]]
+    """
+    An example of how the data object looks like in the end: 
+        [
+          ['Age', 'Weight'],
+          [ 8,      12],
+          [ 4,      5.5],
+          [ 11,     14],
+          [ 4,      5],
+          [ 3,      3.5],
+          [ 6.5,    7]
+        ]
+    The first list will consists of the title of horizontal and vertical axis,
+    and the subsequent list will contain coordinates of the points to be plotted on
+    the google chart
+    """
+
+    #The below for loop is responsible for appending list of two random values  
+    # to data object
+    for i in range(0,11):
         data.append([randrange(101),randrange(101)])
-    # convert data object to JSON
-    h_var_JSON = json.dumps(h_var)
-    v_var_JSON = json.dumps(v_var)
-    modified_data = json.dumps(data)
-    return render(request, 'charts.html', {'values':modified_data, 'h_titlw':h_var_JSON, 'v_title':v_var_JSON})
+
+    #h_var_JSON : JSON string corresponding to  h_var
+    #json.dumps converts Python objects to JSON strings
+    h_var_JSON = dumps(h_var)
+
+    #v_var_JSON : JSON string corresponding to  v_var
+    v_var_JSON = dumps(v_var)
+
+    #modified_data : JSON string corresponding to  data
+    modified_data = dumps(data)
+
+    #Finally all JSON strings are supplied to the charts.html using the 
+    # dictiory shown below so that they can be displayed on the home screen
+    return render(request,"charts.html",{'values':modified_data,\
+        'h_title':h_var_JSON,'v_title':v_var_JSON})
+
+def barras(request):
+    '''
+    data = [
+          ['Jugador', 'Minutos Jugados'],
+          ['Ian', 1000],
+          ['Héctor', 1170],
+          ['Alan', 660],
+          ['Manuel', 1030]
+        ]
+    '''
+    data = []
+    data.append(['Jugador', 'Minutos Jugados'])
+    resultados = Reto.objects.all() #select * from reto;
+    titulo = 'Videojuego Odyssey'
+    titulo_formato = dumps(titulo)
+    subtitulo= 'Total de minutos por jugador'
+    subtitulo_formato = dumps(subtitulo)
+    if len(resultados)>0:
+        for registro in resultados:
+            nombre = registro.nombre
+            minutos = registro.minutos_jugados
+            data.append([nombre,minutos])
+        data_formato = dumps(data) #formatear los datos en string para JSON
+        elJSON = {'losDatos':data_formato,'titulo':titulo_formato,'subtitulo':subtitulo_formato}
+        return render(request,'barras.html',elJSON)
+    else:
+        return HttpResponse("<h1> No hay registros a mostrar</h1>")
